11ea5d340e6534e891477c1d936c97e5d5ad8278
zuma <xjlong@outlook.com>
2019-09-08 22:02:13 +0800

    [ADD]加入pprof监控

--- cmd/all/main.go
+++ cmd/all/main.go
@@ -8,15 +8,25 @@ import (
 	"github.com/0990/avatar-fight-server/game"
 	"github.com/0990/avatar-fight-server/gate"
 	"github.com/sirupsen/logrus"
+	"net/http"
+	_ "net/http/pprof"
 	"os"
 	"os/signal"
+	"runtime"
+	"syscall"
 )
 
 var addr = flag.String("addr", "0.0.0.0:9000", "http service address")
+var pprofAddr = flag.String("pprofAddr", "0.0.0.0:9900", "http pprof service address")
+
+//TODO 加woker性能监控和运行时堆栈打印
 
 func main() {
 	flag.Parse()
 
+	go func() {
+		http.ListenAndServe(*pprofAddr, nil)
+	}()
 	//center
 	err := center.Init(conf.CenterServerID)
 	if err != nil {
@@ -42,5 +52,20 @@ func main() {
 	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, os.Kill)
 	s := <-c
-	fmt.Println("Got signal:", s)
+	logrus.Info("Got signal:", s)
+}
+
+func setupSigusr1Trap() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGINT)
+	go func() {
+		for range c {
+			DumpStacks()
+		}
+	}()
+}
+func DumpStacks() {
+	buf := make([]byte, 16384)
+	buf = buf[:runtime.Stack(buf, true)]
+	fmt.Printf("=== BEGIN goroutine stack dump ===\n%s\n=== END goroutine stack dump ===", buf)
 }
